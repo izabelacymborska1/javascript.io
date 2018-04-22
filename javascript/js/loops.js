b32ab12b46f9e1c26b406ce5d2ef880b04675256
Izabela Cymborska <izabelac@op.pl>
2018-04-22 15:48:05 +0200

    no message

--- javascript/js/loops.js
+++ javascript/js/loops.js
@@ -42,4 +42,44 @@ while (it <10) {
 }
 
 
+
+//pętla do while
+
+    var iter = 20;
+do {
+    console.log(iter);
+    iter++;
+
+} while (iter <10)
+    
+    
+//    przerywanie pętli
+    var a = 0;
+while (a<10) {
+    console.log(++a);
+    
+    if (a == 5) {
+        break;
+    }
+}
+//    przeskakiwanie do kolejnej interacji - contunue
+
+
+for (var b =0; b<10; ++b) {
+    if (b==5) {
+        continue;
+    } else {
+        console.log(b);
+    }
+    console.log("--");
+}
+
+
+    
+    
+    
+    
+    
+    
+    
     
