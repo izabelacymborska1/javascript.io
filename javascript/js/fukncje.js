1ee2a38da0c297b60074d939dac7b5cb65d5707e
Izabela Cymborska <izabelac@op.pl>
2018-04-22 10:48:12 +0200

    no message

---
+++ javascript/js/fukncje.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function aktualnyRok() {
+    console.log("2018");
+}
+
+var aktualnyMiesiac = function() {
+    console.log("Kwiecien");
+}
+
+console.log(aktualnyMiesiac);
+
+aktualnyMiesiac();
+aktualnyMiesiac();
+aktualnyMiesiac();
+aktualnyMiesiac();
+
+
+aktualnyRok();
+aktualnyRok();
