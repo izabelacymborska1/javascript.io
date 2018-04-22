e7ed227bb832c68c78e989465f7521a479a648b4
Izabela Cymborska <izabelac@op.pl>
2018-04-22 12:26:44 +0200

    no message

---
+++ javascript/js/data-types.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//typ liczbowy
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata = wyplata + premia;
+
+calkowitaWyplata = wyplata + premia;
+console.log(calkowitaWyplata);
+
+
+//typ-łancuch znakow- string 
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowtaWyplataStr;
+
+calkowitaWyplataStr = WyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+
+//typ logiczny
+
+var czyJestSmog;
+czyJestSmog = true;
+if (czyJestSmog) {
+    console.log("jest Smog");
+    }
+else {
+    console.log("Nie ma SMOGA");
+}
+
+//typ specjalny nul
+
+var niezdefiniowanaZmienna;
+var nullowZmienna = null;
+console.log(niezdefiniowanaZmienna);
+console.log(nullowZmienna);
+
---
+++ javascript/js/variable-scope.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+
+
+function multiply (l1, l2, l3) {
+    var result = l1 * l2 * l3
+    
+    return result;
+}
+
+
+var wynikMnozenia = multiply (3,4,5);
+
+console.log(wynikMnozenia);
