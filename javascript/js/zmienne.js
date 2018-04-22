be6fb0f2077aaafd44db56854d31a1b5e5a27b08
Izabela Cymborska <izabelac@op.pl>
2018-04-22 10:34:16 +0200

    no message

---
+++ javascript/js/zmienne.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+
+imie = imieDamskie;
+
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+
+
