87689107f40aecff02f28f7fc65c7e7ed628ef77
Izabela Cymborska <izabelac@op.pl>
2018-04-22 16:54:02 +0200

    no message

---
+++ javascript/js/warsztaty-js.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+function multiply (i1, i2, i3) {
+    var result = i1 * i2 * i3;
+    
+    return result;
+}
+
+
+var wynikIloczynu = multiply (5,7,2);
+
+console.log(wynikIloczynu);
+
+
+
+function silnia(10) {
+   if ((n == 0) )
+      return 1
+   else {
+      
+   }
+}
