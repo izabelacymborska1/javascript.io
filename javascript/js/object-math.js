172baacd06d4b07ed77dbe6faa1f85ac17df3759
Izabela Cymborska <izabelac@op.pl>
2018-04-22 14:27:29 +0200

    no message

---
+++ javascript/js/object-math.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//obiekt math
+
+console.log( Math.PI);
+
+console.log(Math.cos(0));
+console.log(Math.random);
+
+
---
+++ javascript/js/operators.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var liczba1 = 13, liczba2 = 5;
+var wynik;
+wynik =  liczba1 % liczba2;
+
+
+wynik +=3;
+
+wynik +=3;
+console.log(wynik);
+
+if (wynik = 7) {
+    console.log("prawda")
+    
+    } else {
+        console.log("fałsz")
+    }
+
+
+(3>2) ? console.log("prawda") : console.log("fałsz");
