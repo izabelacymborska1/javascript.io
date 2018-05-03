7f6b39abd06a5faaccb3f7930c950343f99c431d
Izabela Cymborska <izabelac@op.pl>
2018-05-03 21:07:06 +0200

    no message

---
+++ warsztaty-jquery/animjquery.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+$('button').click(function () {
+
+    $("div").animate({
+        left: "100px",
+        width: "100px",
+        height: "100px"
+    }, 3000).queue(function (next) {
+        $('div').css({
+            backgroundColor: "blue",
+            transition: "5s"
+        });
+
+        });
+
+    });
