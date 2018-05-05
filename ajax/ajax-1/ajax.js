e4bd5f1d611b5fb55f783967ff0d5f723ee84a28
Izabela Cymborska <izabelac@op.pl>
2018-05-05 15:01:52 +0200

    no message

--- ajax/ajax-1/ajax.js
+++ ajax/ajax-1/ajax.js
@@ -1,23 +1,50 @@
-'use strict';
-
-function ajax( method, url ) {
-    //tworze obiekt XMLHttpRequest
-    var httpReq = new XMLHttpRequest();
-    
-    console.log(httpReq);
-    
-//    otwieram połączednie
-    httpReq.open(method,url);
-    
-//    sprawdzam status połęczenia przezdarzenie i nasłuchuję statusu - jak wychwyci 4 to zacznie z tym cos robic   - zdarzenie zmiany statusu połęczenia 
-    
-    httpReq.addEventListener('readystatechange', function() {
+function showData(arrData) {
+    arrData.forEach(function( elem, index) {
+        console.log(elem.title);
         
-        
-        
-    })
-    
-    
+        console.log("===================")
+    });
 }
 
-ajax('GET'
+
+
+
+
+function ajax(method, url) {
+    'use strict';
+    //tworze obiekt XMLHttpRequest
+    var httpReq = new XMLHttpRequest();
+
+//    console.log(httpReq);
+
+    //    otwieram połączednie
+    httpReq.open(method, url);
+
+    //    sprawdzam status połęczenia przezdarzenie i nasłuchuję statusu - jak wychwyci 4 to zacznie z tym cos robic   - zdarzenie zmiany statusu połęczenia 
+
+    httpReq.addEventListener('readystatechange', function () {
+
+        //jesli readystate ==4  - dane zwrócone i gotowe do użycia
+
+        if (httpReq.readyState == 4) {
+            //sprawdzam kod statusu odpwowiedzi - interesuje nas najbardziej 200
+
+            if (httpReq.status == 200) {
+                var data = httpReq.responseText;
+                var jsonData = JSON.parse(data);
+
+                showData( jsonData )
+            }
+        }
+
+    });
+
+
+    httpReq.send();
+
+}
+ajax('GET', "https://jsonplaceholder.typicode.com/posts");
+
+
+//
+//ajax('GET', 'http:echo.jsontest.com/userID/108/userName/Akademia108/userURL/Akademia108.pl');
