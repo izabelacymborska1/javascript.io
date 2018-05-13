08744a1edecfe692beb7ed818c348133108c8b8b
Izabela Cymborska <izabelac@op.pl>
2018-05-13 16:53:05 +0200

    no message

--- bootstrap/bootrstrap-7-prezentacja -ULA/bootstrap-project/js/main.js
+++ bootstrap/bootrstrap-7-prezentacja -ULA/bootstrap-project/js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//smoothy scroll
+
+$(function () {
+    $("a[href*='#']:not([href='#'])").click(function (e) {
+
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    });
+    //csroll nav color change
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $("#main-nav").addClass("scroll");
+        } else {
+            $("#main-nav").removeClass("scroll");
+        }
+
+    });
+
+});
